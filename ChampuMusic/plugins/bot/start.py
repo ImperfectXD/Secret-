6d14f6a0b1303a6c01550682bad37a3efb2fb0e4
𓆩 𝐒 𝐇 𝐔 𝐁 𝐇 𝐎 𓆪 <shubhendugarai917@gmail.com>
2025-03-10 10:11:41 +0530

    Update start.py

--- ChampuMusic/plugins/bot/start.py
+++ ChampuMusic/plugins/bot/start.py
@@ -59,6 +59,7 @@ async def ban_new(client, message):
 async def start_comm(client, message: Message, _):
     chat_id = message.chat.id
     await add_served_user(message.from_user.id)
+    await message.react(random.choice(["🕊️","👀","⚡","🙈"]))
     if len(message.text.split()) > 1:
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
